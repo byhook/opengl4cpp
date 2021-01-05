ab6bee6338f4d40456efb9cf1b0abe6910968c81
handyzhou <byhook@163.com>
2021-01-05 13:47:05 +0800

    绘制第一个窗口

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Mac OS X系统
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    //创建一个窗口对象
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    if (window == NULL) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    //初始化GLAD
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        return -1;
+    }
+
+    //渲染循环-检查是否被要求退出
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //交换颜色缓冲
+        glfwSwapBuffers(window);
+        //事件处理
+        glfwPollEvents();
+    }
+    //染循环结束完成释放
+    glfwTerminate();
     return 0;
 }
