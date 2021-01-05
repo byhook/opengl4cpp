ad90da63c90eee86f053edae5badf546d9ac6736
handyzhou <byhook@163.com>
2021-01-05 15:10:24 +0800

    绘制第一个三角形

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+};
+
+const char *VERTEX_SHADER = "#version 330 core\n"
+                            "layout (location = 0) in vec3 aPos;\n"
+                            "\n"
+                            "void main()\n"
+                            "{\n"
+                            "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                            "}";
+
+//红色、绿色、蓝色和alpha(透明度)分量，通常缩写为RGBA
+const char *FRAGMENT_SHADER = "#version 330 core\n"
+                              "out vec4 FragColor;\n"
+                              "\n"
+                              "void main()\n"
+                              "{\n"
+                              "    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
+                              "} ";
 
 int main() {
     glfwInit();
@@ -21,15 +45,63 @@ int main() {
         return -1;
     }
 
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &VERTEX_SHADER, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &FRAGMENT_SHADER, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    //复制顶点数组到缓冲区中供OpenGL使用
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //设置顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    //顶点属性默认是禁用的，这里开启
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     //渲染循环-检查是否被要求退出
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //激活程序对象
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         //交换颜色缓冲
         glfwSwapBuffers(window);
         //事件处理
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     //染循环结束完成释放
     glfwTerminate();
     return 0;
